702c5b3ed2429dbe7abe5d8c36debb0ca7cf7880
HugoBzn <hugobazan1499@gmail.com>
2023-02-15 14:06:14 -0600

    Header has been created

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <div class="title">
+          <h1>Examen Técnico Programador Web Jr.</h1>
+      </div>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style>
+  body {
+    background-color: #001e26;
+    color: white;
+  }
+  
+  .title {
+      background-color: #012b36;
+      display: flex;
+      justify-content: center;
+      padding: 2% 0 2% 0;
+  }
+  </style>
+  
